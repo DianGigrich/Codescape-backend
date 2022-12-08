d2f4e6dfbca6fcaf222132ba04002f03001368a4
Pooja Agarwal <pooja@Poojas-MacBook-Air.local>
2022-12-08 10:30:23 -0800

    make basic routes

--- models/Question.js
+++ models/Question.js
@@ -1,8 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+// create our ques model
 class Question extends Model {}
 
-Question.init()
+// create fields/columns for ques model
+Question.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'question'
+  }
+);
 
 module.exports = Question;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
