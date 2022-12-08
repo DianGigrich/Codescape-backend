500161d40d012d7342f785ba4ef6a4e328c1d6ab
Machikon <106935371+Machikon@users.noreply.github.com>
2022-12-08 12:47:16 -0800

    pushing to Heroku

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/User');
 
+// GET all users
 router.get('/', async (req, res) => {
   try {
     const userData = await User.findAll();
@@ -9,5 +10,23 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//one user by id
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if(!userData) {
+      res.status(404).json({message: 'No user found with this id.'});
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE a user
+
+
 
 module.exports = router;
